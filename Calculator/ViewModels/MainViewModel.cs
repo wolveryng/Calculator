387d0bd4dace6d237ef94cfa94cef39f63beff31
Borsan Iulian <iulian.borsan@student.unitbv.ro>
2025-03-12 19:00:28 +0200

    Estetic calculator

---
+++ Calculator/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.ViewModels
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        private string _KeyPressedString;
+        public string KeyPressedString
+        {
+            get { return _KeyPressedString; }
+            set
+            {
+                _KeyPressedString = value;
+                OnPropertyChanged("KeyPressedString");
+            }
+        }
+
+        private string _Entered_Number;
+        public string Entered_Number
+        {
+            get { return _Entered_Number; }
+            set
+            {
+                _Entered_Number = value;
+                OnPropertyChanged("Entered_Number");
+            }
+        }
+    }
+}
